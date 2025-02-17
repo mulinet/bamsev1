67521a64d3b1bcd5b9d79f3fdaa15d7b6fd29906
Jisan <mulinet7@gmail.com>
2025-02-17 14:16:12 +0900

    Update login.js

--- renderer/login.js
+++ renderer/login.js
@@ -5,22 +5,19 @@ document.addEventListener("DOMContentLoaded", () => {
     const errorMessage = document.getElementById("errorMessage");
 
     loginButton.addEventListener("click", async () => {
-        const userId = userIdInput.value;
+        const userid = userIdInput.value;
         const password = passwordInput.value;
 
-        if (!userId || !password) {
+        if (!userid || !password) {
             errorMessage.textContent = "아이디와 비밀번호를 입력하세요.";
             errorMessage.style.display = "block";
             return;
         }
 
         try {
-            const response = await window.electronAPI.login({ userId, password });
-
+            const response = await window.electronAPI.login({ userid, password });
             if (response.success) {
-                // ✅ 로그인 성공 → 대시보드로 이동
                 window.location.href = "../views/dashboard.html";
-                // sessionStorage.setItem('token', response.data.token);
             } else {
                 errorMessage.textContent = response.message;
                 errorMessage.style.display = "block";
